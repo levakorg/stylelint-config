da086cd72bc76ddd66a8d7d7e944fa18ccd9cdd7
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-10-23 03:50:25 +0300

    fix() - move packages to folder lib

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-order-config-standard',
-    'stylelint-config-prettier'
-  ],
-  plugins: ['stylelint-order', 'stylelint-prettier'],
-  rules: {
-    'selector-class-pattern': null,
-    'prettier/prettier': true
-  }
+  extends: ['./lib/standard', './lib/order', './lib/prettier']
 }
