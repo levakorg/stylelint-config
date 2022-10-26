01d9426ee0c70c7c090c714d5b7c721c8b067791
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-10-26 03:24:09 +0300

    create(total) - config

---
+++ total/.stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    '../index.js',
+    '../less/index.js',
+    '../scss/index.js',
+    '../sass/index.js',
+    '../stylus/index.js',
+    '../styled-components/index.js'
+  ]
+}
